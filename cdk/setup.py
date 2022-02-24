61f818ab6ace05a771c12cf8f99d8b4c63a45c3f
Ran Isenberg <ran.isenberg@cyberark.com>
2022-02-24 23:35:16 +0200

    remove unused code

--- cdk/setup.py
+++ cdk/setup.py
@@ -6,19 +6,9 @@ from setuptools import find_packages, setup
 # pylint: disable=invalid-name
 here = path.abspath(path.dirname(__file__))
 
-
-def get_micro():
-    build_number: str = getenv('BUILD_NUMBER', '0')
-    branch_name: str = getenv('BRANCH_NAME', 'None')
-    micro = build_number
-    if branch_name != 'master' and not branch_name.startswith('release'):
-        micro = f"dev{build_number}+{''.join(e for e in branch_name if e.isalnum()).lower()}"
-    return micro
-
-
 setup(
     name='aws-lambda-handler-cookbook',
-    version=f'1.0.{get_micro()}',
+    version='1.0.',
     description='CDK code for deploying an AWS Lambda handler that implements the best practices described at https://www.ranthebuilder.cloud',
     classifiers=[
         'Intended Audience :: Developers',
