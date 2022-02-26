1f3ca807c88389a4a84f265c734e8f7d22c71a15
Ran Isenberg <ran.isenberg@cyberark.com>
2022-02-26 17:22:11 +0200

    Add dummy event

--- tests/unit/test_my_handler.py
+++ tests/unit/test_my_handler.py
@@ -5,6 +5,10 @@ from service.handlers.my_handler import my_handler
 from tests.utils import generate_context
 
 
+def generate_event(path_name: str) -> Dict:
+    return {}
+
+
 def test_handler_200_ok():
     response = my_handler({}, generate_context())
     assert response['statusCode'] == HTTPStatus.OK
