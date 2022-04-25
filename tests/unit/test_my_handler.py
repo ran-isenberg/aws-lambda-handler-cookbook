6e59daacfe1f00dc6dd871340b71cccf321c8e12
Ran Isenberg <ran.isenberg@cyberark.com>
2022-04-25 12:30:18 +0300

    fix unit in github

--- tests/unit/test_my_handler.py
+++ tests/unit/test_my_handler.py
@@ -55,6 +55,7 @@ def init():
     os.environ['CONFIGURATION_ENV'] = ENVIRONMENT
     os.environ['CONFIGURATION_NAME'] = CONFIGURATION_NAME
     os.environ['CONFIGURATION_MAX_AGE_MINUTES'] = '5'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'  # used for appconfig mocked boto calls
 
 
 def test_handler_200_ok(mocker):
