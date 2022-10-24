b2db4b03d832659ec4e798e1f0a89470eaa1eaee
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2022-10-24 22:04:32 +0300

    chore: upgrade cdk

--- cdk/setup.py
+++ cdk/setup.py
@@ -25,6 +25,6 @@ setup(
         'aws-cdk-lib>=2.0.0',
         'constructs>=10.0.0',
         'cdk-nag>2.0.0',
-        'aws-cdk.aws-lambda-python-alpha==2.44.0-alpha.0',
+        'aws-cdk.aws-lambda-python-alpha==2.47.0-alpha.0',
     ],
 )
