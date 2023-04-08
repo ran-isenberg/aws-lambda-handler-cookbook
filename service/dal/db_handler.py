5c1a216d9b62d4f8cb1ef87caa2f38353e2f0db6
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-04-08 16:01:44 +0300

    coverage

--- service/dal/db_handler.py
+++ service/dal/db_handler.py
@@ -7,4 +7,4 @@ class DalHandler(ABC):
 
     @abstractmethod
     def create_order_in_db(self, customer_name: str, order_item_count: int) -> OrderEntry:
-        ...
+        ...  # pragma: no cover
