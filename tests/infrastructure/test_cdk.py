f5cb4f5577ea66a9bb81ab0c3d7540e612a2d8c8
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2024-02-03 16:34:09 +0200

    feature: add support for dynamo  v2 (#798)
    * feature: add support for dynamo  v2
    poetry update
    
    ---------
    
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

--- tests/infrastructure/test_cdk.py
+++ tests/infrastructure/test_cdk.py
@@ -14,4 +14,4 @@ def test_synthesizes_properly():
 
     # verify that we have one API GW, that is it not deleted by mistake
     template.resource_count_is('AWS::ApiGateway::RestApi', 1)
-    template.resource_count_is('AWS::DynamoDB::Table', 2)  # main db and one for idempotency
+    template.resource_count_is('AWS::DynamoDB::GlobalTable', 2)  # main db and one for idempotency
