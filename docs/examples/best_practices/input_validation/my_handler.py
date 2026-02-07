55b9a025df743e1656daabec3b28aae4c231e1b6
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2026-02-07 12:08:03 +0200

    feature: add automatic releases (#1023)
    added pr labeler and auto release and other minor updates
    
    ---------
    
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

--- docs/examples/best_practices/input_validation/my_handler.py
+++ docs/examples/best_practices/input_validation/my_handler.py
@@ -11,7 +11,7 @@ from .schema import Input
 def my_handler(event: dict[str, Any], context: LambdaContext):
     try:
         input: Input = parse(event=event, model=Input, envelope=ApiGatewayEnvelope)  # noqa: F841
-    except (ValidationError, TypeError):
+    except ValidationError, TypeError:
         # log error, return BAD_REQUEST
         return {'statusCode': HTTPStatus.BAD_REQUEST}
     # process input
